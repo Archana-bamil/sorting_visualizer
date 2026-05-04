d27dfcfc79bf46cdc7466c899fe872464fd6bf9c
Archana-bamil <archanabamil2006@gmail.com>
2026-05-04 23:58:33 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,130 +1,66 @@
-const cursor=document.querySelector(".cursor");
-document.addEventListener("mousemove",(e)=>{
-    cursor.style .left=e.clientX +"px";
-    cursor.style .top=e.clientY+"px"
+let input=document.querySelector("input");
+console.log(input.value);
+ let size=input.value;
+let array=[size];
+const container=document.querySelector(".container");
+function newarray(){
+ size=input.value;
+  for(let i=0;i<size;i++){
+  let num=Math.random()*(100-0)+1;
+  array[i]=parseInt(Math.floor(num));
+}
+barstyle();
+}
+newarray();
+input.addEventListener("input",()=>{
+  newarray();
 })
-document.addEventListener("mouseleave", () => {
-  cursor.style.display = "none";
-});
-document.addEventListener("mouseenter", () => {
-  cursor.style.display = "block";
-});
-let arraysize=40;
-let array=[arraysize];
-let sortedarray=[arraysize];
-
-
-function generateram(){
-    let num= Math.random()*(100-0)+0;
-    return parseInt(Math.floor(num));
-}
-function generatearray(){
-    for(let i=0;i<arraysize;i++){
-         array[i]=generateram(0,100);
+function bubblesort(array2){
+  const moves=[];
+  for(let i=0;i<size;i++){
+  for(let j=0;j<(size-i);j++){
+  
+    if(array2 [j]>array2[j+1]){
+      moves.push([j+1,j])
+      let temp=array2[j+1];
+      array2[j+1]=array2[j];
+      array2[j]=temp;
     }
+  }
+
 }
-generatearray();
-function newArray(){
-    generatearray();
-    chart.data.datasets[0].data=array;
-    chart.update();
+  return moves;
 }
-function bubblesort(){
-    sortedarray=Array.from(array);
-    for(let i=0;i<arraysize;i++){
-        for(let j=0;j<(arraysize-i);j++){
-           if(sortedarray[j]>sortedarray[j+1]){
-             let temp=sortedarray[j];
-            sortedarray[j]=sortedarray[j+1];
-            sortedarray[j+1]=temp;
-           }
-        }
-    }
-    
-}
-function sortArray(){
-    bubblesort();
-    chart.data.datasets[0].data=sortedarray;
-    chart.update();
+function sortarray(){
+  const copy=[...array];
+const moves=  bubblesort(copy);
+  animate(moves)
 }
 
-let myChart=document.querySelector("#myChart").getContext("2d");
-var chart=new Chart(myChart,{
-    type:"bar",
-    data:{
-        labels:[
-             "Element 1",
-'Element 2',
-'Element 3',
-'Element 4',
-'Element 5',
-'Element 6',
-'Element 7',
-'Element 8',
-'Element 9',
-'Element 10',
-'Element 11',
-'Element 12',
-'Element 13',
-'Element 14',
-'Element 15',
-'Element 16',
-'Element 17',
-'Element 18',
-'Element 19',
-'Element 20',
-'Element 21',
-'Element 22',
-'Element 23',
-'Element 24',
-'Element 25',
-'Element 26',
-'Element 27',
-'Element 28',
-'Element 29',
-'Element 30',
-'Element 31',
-'Element 32',
-'Element 33',
-'Element 34',
-'Element 35',
-'Element 36',
-'ELement 37',
-'Element 38',
-'Element 39',
-'Element 40',
+function animate(moves){
+  if(moves.length==0) {
+    barstyle();
+    return ;
+  }
+const [i,j]=moves.shift();
+  
+    [array[i],array[j]]=[array[j],array[i]];
+  
+  barstyle([i,j]);
+  setTimeout(function(){
+    animate(moves); 
+  },50);
+}
 
-        ],
-datasets:[
-    {
-        label:"Array",
-        data:array,
-        fill:false,
-        borderColor:"#009688",
-       
-        borderWidth:5,
-    },
-],
-},
-options:{
-    responsive:true,
-    maintainAspectRatio:false,
-    scales:{
-        x:{
-            ticks:{
-               autoSkip: false,
-                color:"black",
-        maxRotation: 60,      
-        minRotation: 45
-      }
-            },
-        
-        y:{
-            ticks:{
-                color: "white"
-            },
-        },
-    }
-
-    }}
-)
+function barstyle(indices){
+  container.innerHTML='';
+  for(let i=0;i<size;i++){
+  const bar=document.createElement("div");
+  bar.classList.add("bar");
+  bar.style.height=array[i]+"%";
+  if(indices && indices.includes(i)){
+    bar.style.backgroundColor="red";
+  }
+  container . appendChild(bar);
+}
+}
